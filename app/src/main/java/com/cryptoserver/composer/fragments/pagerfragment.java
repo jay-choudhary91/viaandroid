4cc883344232c01cd2c2a1e6b45005a59eb927c5
Jay Choudhary <jay@matraex.in>
2018-08-07 15:04:39 +0530

    Branch integrated

--- app/src/main/java/com/cryptoserver/composer/fragments/pagerfragment.java
+++ app/src/main/java/com/cryptoserver/composer/fragments/pagerfragment.java
@@ -13,6 +13,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.cryptoserver.composer.R;
 import com.cryptoserver.composer.models.intro;
 
@@ -22,7 +24,8 @@ import com.cryptoserver.composer.models.intro;
  */
 
 public class pagerfragment extends Fragment {
-    ImageView img_image;
+    ImageView img_image,img_gif;
+    WebView webview;
     View RootView;
     boolean isGifLoaded=false;
     int gifFile=0;
@@ -36,35 +39,62 @@ public class pagerfragment extends Fragment {
             TextView txt_description = (TextView) RootView.findViewById(R.id.txt_description);
             TextView btn_start_record = (TextView) RootView.findViewById(R.id.btn_start_record);
             img_image = (ImageView) RootView.findViewById(R.id.img_image);
+            img_gif = (ImageView) RootView.findViewById(R.id.img_gif);
+            webview = (WebView) RootView.findViewById(R.id.webview);
 
-            intro introobject=(intro)getArguments().getParcelable("Object");
+            intro introobject=(intro)getArguments().getParcelable("object");
 
             txt_title.setText(introobject.getTitle());
             txt_description.setText(introobject.getDescription());
-            img_image.setImageResource(introobject.getImage());
+            //img_image.setImageResource(introobject.getImage());
             //tv.setBackgroundColor(Color.parseColor(getArguments().getString("color")));
 
+            if((! isGifLoaded))
+                loadAnimation();
         }
 
         return RootView;
     }
 
 
+    public void loadAnimation()
+    {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                isGifLoaded=true;
+               // webview.loadUrl("file:///android_asset/globe.html");
+                Glide.with(getActivity()).load(R.drawable.globe).diskCacheStrategy(DiskCacheStrategy.SOURCE).
+                        fitCenter().
+                        crossFade().
+                        into(img_gif);
+
+                /*Glide.with(getActivity()).load("file:///android_asset/globe.gif").diskCacheStrategy(DiskCacheStrategy.SOURCE).
+                      fitCenter().
+                      crossFade().
+                      into(img_gif);*/
+
+/*                Glide.with(getContext()).load("file:///android_asset/nicolas.html")
+                        .asGif()
+                        .fitCenter()
+                        .crossFade()
+                        .into(img_gif);*/
+            }
+        },100);
+    }
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
+        if(img_gif != null && (! isGifLoaded))
+            loadAnimation();
     }
 
 
     public static pagerfragment newInstance(intro intro) {
-
-        Object object=null;
         pagerfragment f = new pagerfragment();
         Bundle b = new Bundle();
-        b.putParcelable("Object", (Parcelable) intro);
-
+        b.putParcelable("object", (Parcelable) intro);
         f.setArguments(b);
-
         return f;
     }
 }
