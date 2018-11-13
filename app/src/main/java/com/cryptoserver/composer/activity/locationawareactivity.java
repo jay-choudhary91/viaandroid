1b122dafde4b9ae12fd4345e2731b99afe13777a
Jay Choudhary <jay@matraex.in>
2018-11-13 17:07:25 +0530

    1. Saved audio file meta data. 2. Get frame after audio record.

--- app/src/main/java/com/cryptoserver/composer/activity/locationawareactivity.java
+++ app/src/main/java/com/cryptoserver/composer/activity/locationawareactivity.java
@@ -1135,7 +1135,7 @@ public abstract class locationawareactivity extends baseactivity implements GpsS
             TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
             int networkType = telephonyManager.getNetworkType();
 
-            if(towerinfolist.size()!=0){
+            if(towerinfolist.size() > 0){
                 CellInfo info = towerinfolist.get(0);
                 if (telephonyManager.getSimState() == telephonyManager.SIM_STATE_ABSENT) {
                     Log.e("signalstrenght", "" + mSignalStrength);
