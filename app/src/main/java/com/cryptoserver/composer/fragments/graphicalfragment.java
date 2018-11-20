6b9ea2caadcd3b12173a1ff8ca4771019dbd0ec5
Jay Choudhary <jay@matraex.in>
2018-11-20 15:49:31 +0530

    Change audio format .wav to to m4a (according to iOs app) and check meta data storage.

--- app/src/main/java/com/cryptoserver/composer/fragments/graphicalfragment.java
+++ app/src/main/java/com/cryptoserver/composer/fragments/graphicalfragment.java
@@ -194,7 +194,7 @@ public class graphicalfragment extends basefragment implements
     private Runnable waveRunnable;
     noise mNoise;
     boolean isgraphicopen=false,photocapture=false;
-    public String currenthashvalue="";
+    public String currenthashvalue="",lastsavedangle="";
     boolean ismediaplayer = false;
     private boolean isinbackground=false;
     ArrayList<LatLng> points=new ArrayList<>();
@@ -203,28 +203,6 @@ public class graphicalfragment extends basefragment implements
     private static final PatternItem DOT = new Dot();
     private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
     private static final List<PatternItem> PATTERN_POLYLINE_DOTTED = Arrays.asList(GAP, DOT);
-
-    /*private static final int COLOR_BLACK_ARGB = 0xff000000;
-    private static final int COLOR_WHITE_ARGB = 0xffffffff;
-    private static final int COLOR_GREEN_ARGB = 0xff388E3C;
-    private static final int COLOR_PURPLE_ARGB = 0xff81C784;
-    private static final int COLOR_ORANGE_ARGB = 0xffF57F17;
-    private static final int COLOR_BLUE_ARGB = 0xffF9A825;
-
-    private static final int POLYGON_STROKE_WIDTH_PX = 8;
-    private static final int PATTERN_DASH_LENGTH_PX = 20;
-    private static final int PATTERN_GAP_LENGTH_PX = 20;
-    private static final PatternItem DOT = new Dot();
-    private static final PatternItem DASH = new Dash(PATTERN_DASH_LENGTH_PX);
-    private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
-
-    // Create a stroke pattern of a gap followed by a dash.
-    private static final List<PatternItem> PATTERN_POLYGON_ALPHA = Arrays.asList(GAP, DASH);
-
-    // Create a stroke pattern of a dot followed by a gap, a dash, and another gap.
-    private static final List<PatternItem> PATTERN_POLYGON_BETA =
-            Arrays.asList(DOT, GAP, DASH, GAP);*/
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -332,11 +310,16 @@ public class graphicalfragment extends basefragment implements
                             if(xdata.getinstance().getSetting(config.orientation).toString().trim().length() > 0)
                             {
                                 String strdegree=xdata.getinstance().getSetting(config.orientation);
-                                if(strdegree.equalsIgnoreCase("NA"))
-                                    strdegree="0.0";
+                                if(! strdegree.equals(lastsavedangle))
+                                {
+                                    if(strdegree.equalsIgnoreCase("NA"))
+                                        strdegree="0.0";
 
-                                int degree = Math.abs((int)Double.parseDouble(strdegree));
-                                rotatecompass(degree);
+                                    int degree = Math.abs((int)Double.parseDouble(strdegree));
+                                    rotatecompass(degree);
+                                }
+                                lastsavedangle=strdegree;
+
                             }
                         }
                     }
