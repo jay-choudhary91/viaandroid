7d4f9abe5ac3f8fed4f2006066ed066801a1ee86
akhilesh singh <akhilesh1010@gmail.com>
2018-12-19 18:28:58 +0530

    resolved:cpuusageuser format for video is not correct.

--- app/src/main/java/com/cryptoserver/composer/activity/locationawareactivity.java
+++ app/src/main/java/com/cryptoserver/composer/activity/locationawareactivity.java
@@ -1351,6 +1351,11 @@ public abstract class locationawareactivity extends baseactivity implements GpsS
             public void run() {
                 try {
                     String system = common.executeTop();
+                    if(system.contains("Tasks")){
+                        system=system.substring(system.indexOf("Tasks"),system.length());
+                        system=  system.replace("Tasks:","");
+                    }
+                    Log.e("system",system);
                     String[] cpuArray = system.split(",");
                     final String[] value1 = {cpuArray[0]};
                     final String[] value2 = {cpuArray[1]};
