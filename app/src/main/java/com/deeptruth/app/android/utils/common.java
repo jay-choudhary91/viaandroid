6aa36a1d0a85144b1de86c33094e1ed2c62c1fd0
devesh kumawat <devesh@matraex.in>
2019-01-28 18:21:16 +0530

    add linechart in drawer

--- app/src/main/java/com/deeptruth/app/android/utils/common.java
+++ app/src/main/java/com/deeptruth/app/android/utils/common.java
@@ -2405,7 +2405,7 @@ public class common {
             encryptionstring.setSpan(new StyleSpan(semiboldfonttype.getStyle()),encryption_key.length(),encryptionstr.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             encryptionstring.setSpan(new RelativeSizeSpan(1.1f),encryption_key.length(),encryptionstr.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             txt_encryption.setText(encryptionstring);
-            
+
     }
 
     public static String filesize(String imageurl) {
