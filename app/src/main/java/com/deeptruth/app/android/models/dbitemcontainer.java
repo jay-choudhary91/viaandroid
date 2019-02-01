b15e08de5ac4a7857e67b4ff26388b0930b60b12
Jay Choudhary <jay@matraex.in>
2019-02-01 19:31:11 +0530

    Data display issues in reader app in all the sections.

--- app/src/main/java/com/deeptruth/app/android/models/dbitemcontainer.java
+++ app/src/main/java/com/deeptruth/app/android/models/dbitemcontainer.java
@@ -29,71 +29,71 @@ public class dbitemcontainer implements Serializable
     public String item19="";
     public String item20="";
 
-
     public dbitemcontainer(String item1,String item2,String item3)
     {
-        this.item1=item1;
-        this.item2=item2;
-        this.item3=item3;
+        setItem1(item1);
+        setItem2(item2);
+        setItem3(item3);
     }
 
     public dbitemcontainer(String item1,String item2,String item3,String item4,String item5,String item6,String item7,String item8
             ,String item9,String item10,String item11,String item12,String item13)
     {
-        this.item1=item1;
-        this.item2=item2;
-        this.item3=item3;
-        this.item4=item4;
-        this.item5=item5;
-        this.item6=item6;
-        this.item7=item7;
-        this.item8=item8;
-        this.item9=item9;
-        this.item10=item10;
-        this.item11=item11;
-        this.item12=item12;
-        this.item13=item13;
+        setItem1(item1);
+        setItem2(item2);
+        setItem3(item3);
+        setItem4(item4);
+        setItem5(item5);
+        setItem6(item6);
+        setItem7(item7);
+        setItem8(item8);
+        setItem9(item9);
+        setItem10(item10);
+        setItem11(item11);
+        setItem12(item12);
+        setItem13(item13);
     }
 
     public dbitemcontainer(String item1,String item2,String item3,String item4,String item5,String item6,String item7,String item8
             ,String item9,String item10,String item11,String item12,String item13,String item14)
     {
-        this.item1=item1;
-        this.item2=item2;
-        this.item3=item3;
-        this.item4=item4;
-        this.item5=item5;
-        this.item6=item6;
-        this.item7=item7;
-        this.item8=item8;
-        this.item9=item9;
-        this.item10=item10;
-        this.item11=item11;
-        this.item12=item12;
-        this.item13=item13;
-        this.item14=item14;
+        setItem1(item1);
+        setItem2(item2);
+        setItem3(item3);
+        setItem4(item4);
+        setItem5(item5);
+        setItem6(item6);
+        setItem7(item7);
+        setItem8(item8);
+        setItem9(item9);
+        setItem10(item10);
+        setItem11(item11);
+        setItem12(item12);
+        setItem13(item13);
+        setItem14(item14);
     }
 
 
     public dbitemcontainer(String item1,String item2,String item3,String item4,String item5,String item6,String item7,String item8
             ,String item9,String item10,String item11,String item12,String item13,String item14,String item15)
     {
-        this.item1=item1;
-        this.item2=item2;
-        this.item3=item3;
-        this.item4=item4;
-        this.item5=item5;
-        this.item6=item6;
-        this.item7=item7;
-        this.item8=item8;
-        this.item9=item9;
-        this.item10=item10;
-        this.item11=item11;
-        this.item12=item12;
-        this.item13=item13;
-        this.item14=item14;
-        this.item15=item15;
+        setItem1(item1);
+        setItem2(item2);
+        setItem3(item3);
+        setItem4(item4);
+        setItem5(item5);
+        setItem6(item6);
+        setItem7(item7);
+        setItem8(item8);
+        setItem9(item9);
+        setItem10(item10);
+        setItem11(item11);
+        setItem12(item12);
+        setItem13(item13);
+        setItem14(item14);
+        setItem15(item15);
     }
+
 
     public String getItem1() {
         return item1;
