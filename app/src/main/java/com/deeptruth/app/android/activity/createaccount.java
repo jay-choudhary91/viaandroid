5c77bf4ea9d571e111e28177730f969834ee5f7f
devesh kumawat <devesh@matraex.in>
2019-04-04 18:53:40 +0530

    Merge branch 'development-devesh-2019-04-04' of http://git.matraex.com/matraexdevelopment/viaandroid into development-sonal-2019-04-04
    # Conflicts:
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/com/deeptruth/app/android/activity/createaccount.java
+++ app/src/main/java/com/deeptruth/app/android/activity/createaccount.java
@@ -1,16 +1,88 @@
 package com.deeptruth.app.android.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.deeptruth.app.android.R;
+import com.deeptruth.app.android.utils.common;
+import com.github.reinaldoarrosi.maskededittext.MaskedEditText;
+import com.google.android.gms.common.internal.service.Common;
 
-public class createaccount extends Activity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class createaccount extends Activity implements View.OnClickListener  {
+
+
+    @BindView(R.id.edt_username)
+    EditText edt_username;
+    @BindView(R.id.email)
+    EditText edt_email;
+    @BindView(R.id.edt_password)
+    EditText edt_password;
+    @BindView(R.id.edt_confirmpassword)
+    EditText edt_confirmpassword;
+    @BindView(R.id.edt_phonenumber)
+    MaskedEditText edt_phonenumber;
+    @BindView(R.id.txt_submit)
+    TextView txt_submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         setContentView(R.layout.activity_createaccountactivity);
+        ButterKnife.bind(this);
+
+        txt_submit.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.txt_submit:
+                if(checkValidations() == true){
+                    Intent intent=new Intent(createaccount.this,verifiedemail.class);
+                    startActivity(intent);
+                }
+
+
+                break;
+        }
+    }
+
+        public  boolean checkValidations() {
+
+            if (edt_username.getText().toString().trim().toString().length() == 0) {
+                Toast.makeText(this, "Please enter user name!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (! common.isValidEmail(edt_email.getText().toString().trim())) {
+                Toast.makeText(this, "Please enter valid email!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (edt_password.getText().toString().trim().toString().length() == 0) {
+                Toast.makeText(this, "Please enter password!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (edt_confirmpassword.getText().toString().trim().toString().length() == 0) {
+                Toast.makeText(this, "Please enter confirm password!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (!(edt_password.getText().toString().trim().toString()).equals(edt_confirmpassword.getText().toString().trim().toString())) {
+                Toast.makeText(this, " confirm password is not correct!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            return true;
+        }
 }
--- app/src/main/java/com/deeptruth/app/android/activity/signin.java
+++ app/src/main/java/com/deeptruth/app/android/activity/signin.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -32,13 +33,11 @@ public class signin extends Activity implements View.OnClickListener{
     @BindView(R.id.forgot_password)
     TextView txt_forgotpassword;
 
-    String username , password;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginactivity);
         ButterKnife.bind(this);
-
 
         txt_login.setOnClickListener(this);
         txt_createaccount.setOnClickListener(this);
@@ -54,29 +53,15 @@ public class signin extends Activity implements View.OnClickListener{
            case R.id.createaccount:
                Intent intent=new Intent(signin.this,createaccount.class);
                startActivity(intent);
-               finish();
                break;
            case R.id.forgot_password:
                intent = new Intent(signin.this, forgotpassword.class);
                startActivity(intent);
-               finish();
                break;
 
        }
     }
     public void login(){
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if(BuildConfig.FLAVOR.equalsIgnoreCase(config.build_flavor_reader))
-                {
-                    Intent in=new Intent(signin.this,homeactivity.class);
-                    startActivity(in);
-                    finish();
-                }
-                else
-                {
-
                     if(xdata.getinstance().getSetting(config.enableintroscreen).isEmpty() || xdata.getinstance().getSetting(config.enableintroscreen).equalsIgnoreCase("yes"))
                     {
                         if(xdata.getinstance().getSetting(config.enableintroscreen).isEmpty())
@@ -92,12 +77,6 @@ public class signin extends Activity implements View.OnClickListener{
                         overridePendingTransition(R.anim.activityfadein, R.anim.activityfadeout);
                         finish();
                     }
-                }
-            }
-        },1000);
     }
 
-    public void validation(){
-
-    }
 }
