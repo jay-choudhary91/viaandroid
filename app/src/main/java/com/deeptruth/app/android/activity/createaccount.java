b4ebb6d0cb78d134af8a0ace04e21f2b73ab472e
devesh kumawat <devesh@matraex.in>
2019-04-04 12:29:15 +0530

    create activity for screen

---
+++ app/src/main/java/com/deeptruth/app/android/activity/createaccount.java
@@ -0,0 +1,16 @@
+package com.deeptruth.app.android.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.deeptruth.app.android.R;
+
+public class createaccount extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createaccountactivity);
+    }
+}
---
+++ app/src/main/java/com/deeptruth/app/android/activity/signin.java
@@ -0,0 +1,16 @@
+package com.deeptruth.app.android.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.deeptruth.app.android.R;
+
+public class signin extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginactivity);
+    }
+}
