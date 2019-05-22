d5301ea0d2cfb4c6dc02790cd9a716160679259a
devesh kumawat <devesh@matraex.in>
2019-05-22 20:15:09 +0530

    zoominout animation

--- app/src/main/java/com/deeptruth/app/android/fragments/composeoptionspagerfragment.java
+++ app/src/main/java/com/deeptruth/app/android/fragments/composeoptionspagerfragment.java
@@ -11,6 +11,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
@@ -30,6 +32,7 @@ import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -123,6 +126,9 @@ public class composeoptionspagerfragment extends basefragment implements View.On
     Typeface fontfaceregular, fontfacebold;
     private String OPEN_SANS_BOLD = "fonts/OpenSans-Bold.ttf";
     private String OPEN_SANS_REGULAR = "fonts/OpenSans-Regular.ttf";
+    private Animator currentAnimator;
+    private int shortAnimationDuration;
+    boolean zoominout = false;
 
     @Override
     public int getlayoutid() {
@@ -434,7 +440,7 @@ public class composeoptionspagerfragment extends basefragment implements View.On
         currentselectedcomposer=config.selectedmediatype;
         pagermediatype.setCurrentItem(currentselectedcomposer,true);
 
-        getlatestthumbnailfromdirectory();
+        getlatestthumbnailfromdirectory(false);
 
         int currentpagerpos=currentselectedcomposer;
         if(currentpagerpos == 0)
@@ -724,7 +730,7 @@ public class composeoptionspagerfragment extends basefragment implements View.On
             break;
         }
 
-        setimagethumbnail();
+        setimagethumbnail(false);
     }
 
     adapteritemclick mitemclick=new adapteritemclick() {
@@ -785,7 +791,7 @@ public class composeoptionspagerfragment extends basefragment implements View.On
             {
                 e.printStackTrace();
             }
-            getlatestthumbnailfromdirectory();
+            getlatestthumbnailfromdirectory(true);
             xdata.getinstance().saveSetting(config.istravelleddistanceneeded,"false");
         }
         else if(type == adjustzoom)
@@ -810,7 +816,7 @@ public class composeoptionspagerfragment extends basefragment implements View.On
             blinkencryptionanimation.cancel();
     }
 
-    public void getlatestthumbnailfromdirectory()
+    public void getlatestthumbnailfromdirectory(boolean zoominoutimage)
     {
         audioarraylist.clear();
         videoarraylist.clear();
@@ -866,14 +872,14 @@ public class composeoptionspagerfragment extends basefragment implements View.On
 
         try
         {
-            setimagethumbnail();
+            setimagethumbnail(zoominoutimage);
         }catch (Exception e)
         {
             e.printStackTrace();
         }
     }
 
-    public void setimagethumbnail()
+    public void setimagethumbnail(boolean zoominoutimage)
     {
         img_mediathumbnail.setImageResource(0);
         img_mediathumbnail.setBackgroundColor(applicationviavideocomposer.getactivity().getResources().getColor(R.color.transparent));
@@ -882,7 +888,7 @@ public class composeoptionspagerfragment extends basefragment implements View.On
         {
             if(videoarraylist.size() > 0)
             {
-                setthumbnailimage(videoarraylist.get(0));
+               setthumbnailimage(videoarraylist.get(0),zoominoutimage);
             }
             else
             {
@@ -893,7 +899,7 @@ public class composeoptionspagerfragment extends basefragment implements View.On
         {
             if(imagearraylist.size() > 0)
             {
-                setthumbnailimage(imagearraylist.get(0));
+                setthumbnailimage(imagearraylist.get(0),zoominoutimage);
             }
             else
             {
@@ -904,7 +910,7 @@ public class composeoptionspagerfragment extends basefragment implements View.On
         {
             if(audioarraylist.size() > 0)
             {
-                setthumbnailimage(audioarraylist.get(0));
+                setthumbnailimage(audioarraylist.get(0),zoominoutimage);
                 img_mediathumbnail.setBackgroundColor(applicationviavideocomposer.getactivity().getResources().getColor(R.color.dark_blue_solid_a));
             }
             else
@@ -914,13 +920,18 @@ public class composeoptionspagerfragment extends basefragment implements View.On
         }
     }
 
-    public void setthumbnailimage(String mediapath)
+    public void setthumbnailimage(String mediapath,boolean zoominoutimage)
     {
         Uri uri= FileProvider.getUriForFile(applicationviavideocomposer.getactivity(),
                 BuildConfig.APPLICATION_ID + ".provider", new File(mediapath));
-        Glide.with(applicationviavideocomposer.getactivity()).load(uri).thumbnail(0.1f)
+        if(! zoominoutimage){
+          Glide.with(applicationviavideocomposer.getactivity()).load(uri).thumbnail(0.1f)
                 .transition(GenericTransitionOptions.with(R.anim.fadein)).
                 into(img_mediathumbnail);
+        }else{
+            zoomImageFromThumb(img_mediathumbnail,uri);
+            shortAnimationDuration = 400;
+        }
     }
 
     public void showhideactionbottombaricon(int i){
@@ -1082,5 +1093,160 @@ public class composeoptionspagerfragment extends basefragment implements View.On
         animatorSet.playTogether(cornerAnimation, shiftAnimation);
         animatorSet.start();
         iscircle = !iscircle;
+    }
+
+    private void zoomImageFromThumb(final ImageView thumbView, final Uri uri) {
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        if (currentAnimator != null) {
+            currentAnimator.cancel();
+        }
+
+        // Load the high-resolution "zoomed-in" image.
+        final ImageView expandedImageView = (ImageView) findViewById(R.id.expanded_image);
+        Glide.with(applicationviavideocomposer.getactivity()).load(uri).thumbnail(0.1f)
+               .transition(GenericTransitionOptions.with(R.anim.fadein)).
+                into(expandedImageView);
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        // This step involves lots of math. Yay, math.
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Also set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.container)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        // Adjust the start bounds to be the same aspect ratio as the final
+        // bounds using the "center crop" technique. This prevents undesirable
+        // stretching during the animation. Also calculate the start scaling
+        // factor (the end scaling factor is always 1.0).
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            Log.e("startscale",""+startScale);
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it will position the zoomed-in view in the place of the
+        // thumbnail.
+        expandedImageView.setVisibility(View.INVISIBLE);
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f))
+                .with(ObjectAnimator.ofFloat(expandedImageView,
+                        View.SCALE_Y, startScale, 1f));
+        set.setDuration(shortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                currentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                currentAnimator = null;
+            }
+        });
+        set.start();
+        currentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        final float startScaleFinal = startScale;
+        Log.e("startscale",""+startScaleFinal);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setonmediathumbnail(expandedImageView,startBounds,startScaleFinal,thumbView,uri);
+                expandedImageView.setVisibility(View.VISIBLE);
+            }
+        },500);
+
+
+    }
+
+    public void setonmediathumbnail(final ImageView expandedImageView, Rect startBounds, float startScaleFinal, final ImageView thumbView, final Uri uri){
+        if (currentAnimator != null) {
+            currentAnimator.cancel();
+        }
+
+        // Animate the four positioning/sizing properties in parallel,
+        // back to their original values.\
+        AnimatorSet set = new AnimatorSet();
+        set.play(ObjectAnimator
+                .ofFloat(expandedImageView, View.X, startBounds.left))
+                .with(ObjectAnimator
+                        .ofFloat(expandedImageView,
+                                View.Y,startBounds.top))
+                .with(ObjectAnimator
+                        .ofFloat(expandedImageView,
+                                View.SCALE_X, startScaleFinal))
+                .with(ObjectAnimator
+                        .ofFloat(expandedImageView,
+                                View.SCALE_Y, startScaleFinal));
+        set.setDuration(600);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                thumbView.setAlpha(1f);
+                Glide.with(applicationviavideocomposer.getactivity()).load(uri).thumbnail(0.1f)
+                        .transition(GenericTransitionOptions.with(R.anim.fadein)).
+                        into(thumbView);
+                expandedImageView.setVisibility(View.GONE);
+                currentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                thumbView.setAlpha(1f);
+                Glide.with(applicationviavideocomposer.getactivity()).load(uri).thumbnail(0.1f)
+                        .transition(GenericTransitionOptions.with(R.anim.fadein)).
+                        into(thumbView);
+                expandedImageView.setVisibility(View.GONE);
+                currentAnimator = null;
+            }
+        });
+        set.start();
+        currentAnimator = set;
     }
 }
