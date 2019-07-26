191fa581448c4244a398f5961ceb6d77b9dd0441
devesh kumawat <devesh@matraex.in>
2019-07-26 20:36:14 +0530

    Merge branch 'development-jay-2019-07-26' of http://git.matraex.com/matraexdevelopment/viaandroid into development-sonal-2019-07-26
    # Conflicts:
    #	app/src/main/java/com/deeptruth/app/android/activity/baseactivity.java

--- app/src/main/java/com/deeptruth/app/android/activity/baseactivity.java
+++ app/src/main/java/com/deeptruth/app/android/activity/baseactivity.java
@@ -1914,49 +1914,6 @@ public abstract class baseactivity extends AppCompatActivity implements basefrag
         uploadFile.execute();
     }
 
-    public void getscreenwidthheight(Dialog dialog,int widthpercentage,int heightpercentage) {
-
-    /* Callback used for interactive request.  If succeeds we use the access
-     * token to call the Microsoft Graph. Does not check cache
-     */
-    private AuthenticationCallback getAuthInteractiveCallback() {
-        return new AuthenticationCallback() {
-
-            @Override
-            public void onSuccess(IAuthenticationResult authenticationResult) {
-                /* Successfully got a token, call graph now */
-                Log.d(TAG, "Successfully authenticated");
-                Log.d(TAG, "ID Token: " + authenticationResult.getIdToken());
-
-                /* Store the auth result */
-                onedriveresult = authenticationResult;
-
-                /* call graph */
-                callGraphAPI();
-
-                /* update the UI to post call graph state */
-               // updateSuccessUI();
-            }
-
-            @Override
-            public void onError(MsalException exception) {
-                /* Failed to acquireToken */
-
-             //   Log.e("MsalException ",exception.getMessage());
-                /*if (exception instanceof MsalClientException) {
-                    *//* Exception inside MSAL, more info inside MsalError.java *//*
-                } else if (exception instanceof MsalServiceException) {
-                    *//* Exception when communicating with the STS, likely config issue *//*
-                }*/
-            }
-
-            @Override
-            public void onCancel() {
-                /* User canceled the authentication */
-                Log.d(TAG, "User cancelled login.");
-            }
-        };
-    }
 
     public void setscreenwidthheight(Dialog dialog,int widthpercentage,int heightpercentage,String gravity) {
 
